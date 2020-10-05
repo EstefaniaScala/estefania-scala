58416d8e50200a3a789a943d960f43ba3c7fe881
Oswaldo Zarate <2448407-hiwaldo89@users.noreply.gitlab.com>
2020-10-04 19:59:44 -0500

    add coming soon

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,34 +8,6 @@ import Header from "./header"
 const Layout = ({ children }) => {
   return (
     <div>
-      <Global
-        styles={css`
-          body,
-          html {
-            font-family: "Karla", sans-serif;
-            box-sizing: border-box;
-          }
-          .container {
-            padding-left: 15px;
-            padding-right: 15px;
-            width: 100%;
-            margin: auto;
-            @media (min-width: 576px) {
-              max-width: 540px;
-            }
-            @media (min-width: 768px) {
-              max-width: 720px;
-            }
-            @media (min-width: 992px) {
-              max-width: 960px;
-            }
-            @media (min-width: 1200px) {
-              max-width: 1140px;
-            }
-          }
-        `}
-      />
-      <Header />
       <main>{children}</main>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,34 @@
 import React from "react"
+import { css } from "@emotion/core"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Cover from "../components/Cover"
+import Logo from "../images/logo.svg"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Estefanía Escala" />
-    <Cover />
+    <SEO title="Coming Soon" />
+    <div
+      css={css`
+        * {
+          box-sizing: border-box;
+          margin: 0;
+          padding: 0;
+        }
+        width: 100%;
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 0 15px;
+        img {
+          width: 100%;
+          max-width: 400px;
+        }
+      `}
+    >
+      <img src={Logo} alt="Estefanía Scala" />
+    </div>
   </Layout>
 )
 
